7156ae51c492cf8348421b65e5faa4c8dab44b7c
Jonathan Loescher <jonathan@jloescher.com>
2023-03-26 17:09:08 -0400

    Updated Collections Updated URLs Updated Views *** Added like functionality

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MusicListCreateView, MusicRetrieveUpdateDestroyView
+from .views import MusicListCreateView, MusicRetrieveUpdateDestroyView, MusicLikeView
 
 urlpatterns = [
     path('music/', MusicListCreateView.as_view(), name='music-list-create'),
     path('music/<int:pk>/', MusicRetrieveUpdateDestroyView.as_view(),
          name='music-retrieve-update-destroy'),
+    path('music/<int:pk>/like/', MusicLikeView.as_view(), name='music-like-update')
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics
+from rest_framework import generics, serializers, status
+from rest_framework.response import Response
 from .models import Music
 from .serializers import MusicSerializer
 
@@ -8,6 +9,22 @@ from .serializers import MusicSerializer
 class MusicListCreateView(generics.ListCreateAPIView):
     queryset = Music.objects.all()
     serializer_class = MusicSerializer
+
+    def like(self, request, pk=None):
+        music = self.get_object()
+        music.likes += 1
+        music.save()
+        return Response({'likes': music.likes})
+
+
+class MusicLikeView(generics.GenericAPIView):
+    queryset = Music.objects.all()
+
+    def post(self, request, pk=None):
+        music = self.get_object()
+        music.likes += 1
+        music.save()
+        return Response({'likes': music.likes}, status=status.HTTP_200_OK)
 
 
 class MusicRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
